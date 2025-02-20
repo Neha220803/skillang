b98a79e2a52c6a7e967dcc4e41950146972d22af
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-02-20 17:54:10 +0530

    added salary ranges

--- src/components/landing/nurse-in-germany/nurseStats.js
+++ src/components/landing/nurse-in-germany/nurseStats.js
@@ -10,23 +10,34 @@ const NurseStats = () => {
       <Container className='w-100 d-flex justify-content-center align-items-center'>
         <Row  className='nurseStatsbg py-4 ' >
           
-            <Col lg={4} sm={12} xs={12} className='d-flex flex-column '>
+            <Col lg={5} sm={12} xs={12} className='d-flex flex-column '>
           <div className='compSal d-flex flex-row'>
             <img src={walletIcon} alt='wallet' className='image-fluid' />
             <div className='text-primary-inverse caption-medium'>Competitive Salaries</div>
           </div>
           <div className='subheading-small-medium text-content-secondary'>Before Recognition</div>
-          <div className='subheading-big-medium text-content-secondary'>
+          <div  className='d-flex align-items-end'>
+             <div className='subheading-big-medium text-content-secondary me-1'>
             €2,200 - €2,400 PM
           </div>
+          <div className=''>+Benefits per month</div>
+
+          </div>
+          
           </Col>
-          <Col lg={4} sm={12} xs={12} className='d-flex flex-column align-items-start justify-content-end mt-4'>
+          <Col lg={5} sm={12} xs={12} className='d-flex flex-column align-items-start justify-content-end mt-4'>
           <div className='subheading-small-medium text-content-secondary'>After Recognition</div>
-          <div className='subheading-big-medium' style={{color:'#4F8AFB'}}>€3,200 - €3,400 PM</div>
+          <div style={{color:'#4F8AFB'}} className='d-flex align-items-end'>
+             <div className='subheading-big-medium' >€3,200 - €3,400 PM</div>
+          <div className='text-content-secondary'>+Benefits per month</div>
+
+          </div>
+         
           </Col>
-          <Col lg={4}>
+          <Col lg={2}>
             
           </Col>
+          <div className='caption-regular mt-4'>*Salaries may vary depending on region, experience, and specialization.</div>
       
         </Row>
       </Container>
