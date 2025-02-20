60832b48eaa7e0db7711f50185844f9063171b53
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-02-20 19:31:29 +0530

    responsive logo

--- src/components/landing/landing-nav/landing-nav.js
+++ src/components/landing/landing-nav/landing-nav.js
@@ -3,6 +3,8 @@ import { Container, Image } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../../assets/images/logo-3.svg";
 import { FiPhoneCall } from "react-icons/fi";
+import '../../../index.css';
+import '../../../App.css';
 
 const LandingNavBar = () => {
   return (
@@ -10,16 +12,19 @@ const LandingNavBar = () => {
       <Container className="d-flex align-items-center justify-content-between">
         {/* Logo on the left */}
         <Navbar.Brand>
-          <Image src={logo} className="navbar-logo" />
+          <Image
+            src={logo}
+            className="navbar-logo"
+          />
         </Navbar.Brand>
         {/* Push button to the right */}
          <button
-            className="btn btn-primary-outline d-flex align-items-center justify-content-center fw-bold"
+            className="btn btn-primary-outline d-flex align-items-center justify-content-center"
             onClick={() => {
                 window.location.href = "tel:+917200630336";
             }}
             >
-            <FiPhoneCall className="me-2" style={{ width: "24px", height: "24px" }} />
+            <FiPhoneCall className="me-2" style={{ width: "20px", height: "20px" }} />
             91 - 7200 630 336
             </button>
       </Container>
