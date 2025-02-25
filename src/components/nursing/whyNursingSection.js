9412a1768d3d50afebc183ce37f2a7c9daf81501
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-02-25 22:38:53 +0530

    push

--- src/components/nursing/whyNursingSection.js
+++ src/components/nursing/whyNursingSection.js
@@ -4,15 +4,21 @@ import { Container, Col, Row, Card, CardBody, CardImg } from "react-bootstrap";
 import '../../index.css';
 import '../../App.css';
 import whyNurse1 from '../../assets/icons/nurse/whyNurse1.svg';
+import whyNurse2 from '../../assets/icons/nurse/whyNurse2.svg';
+import whyNurse3 from '../../assets/icons/nurse/whyNurse3.svg';
+import whyNurse4 from '../../assets/icons/nurse/whyNurse4.svg';
+import whyNurse5 from '../../assets/icons/nurse/whyNurse5.svg';
+import whyNurse6 from '../../assets/icons/nurse/whyNurse6.svg';
+import whyNurse7 from '../../assets/icons/nurse/whyNurse7.svg';
 
 const WhyNurseCardData = [
     { title: "High Demand in Healthcare", desc: "Free German Courses, Online & In-Person Training & Flexible Learning Schedules all in One Place", img: whyNurse1, color: "#FFFBFB" },
-    { title: "Advanced Medical Technology", desc: "Working with cutting-edge medical equipment and innovative practices enhances professional development", img: whyNurse1, color: "#FFFBF3" },
-    { title: "Career Growth & Specialization", desc: "Healthcare careers offer continuous opportunities for growth, specialization, and upward mobility in a various fields.", img: whyNurse1, color: "#F8FAFF" },
-    { title: "Competitive Salary & Benefits", desc: "Healthcare professionals enjoy attractive salary packages and comprehensive benefits, ensuring financial stability", img: whyNurse1, color: "#F9F7FF" },
-    { title: "Work-Life Balance", desc: "Enjoy a healthy work-life balance in regions, making it easier to maintain both career and personal well-being", img: whyNurse1, color: "#F8FAFF" },
-    { title: "Language Support for Integration", desc: "Language programs make it easier for healthcare professionals to integrate into new communities", img: whyNurse1, color: "#FFF9FD" },
-    { title: "Healthcare Worker Rights", desc: "Healthcare systems prioritize the wellbeing and rights of their workers, ensuring a supportive and secure", img: whyNurse1, color: "#FFFBF3" },
+    { title: "Advanced Medical Technology", desc: "Working with cutting-edge medical equipment and innovative practices enhances professional development", img: whyNurse2, color: "#FFFBF3" },
+    { title: "Career Growth & Specialization", desc: "Healthcare careers offer continuous opportunities for growth, specialization, and upward mobility in a various fields.", img: whyNurse3, color: "#F8FAFF" },
+    { title: "Competitive Salary & Benefits", desc: "Healthcare professionals enjoy attractive salary packages and comprehensive benefits, ensuring financial stability", img: whyNurse4, color: "#F9F7FF" },
+    { title: "Work-Life Balance", desc: "Enjoy a healthy work-life balance in regions, making it easier to maintain both career and personal well-being", img: whyNurse5, color: "#F8FAFF" },
+    { title: "Language Support for Integration", desc: "Language programs make it easier for healthcare professionals to integrate into new communities", img: whyNurse6, color: "#FFF9FD" },
+    { title: "Healthcare Worker Rights", desc: "Healthcare systems prioritize the wellbeing and rights of their workers, ensuring a supportive and secure", img: whyNurse7, color: "#FFFBF3" },
     { title: "Vibrant & Cultural Cities", desc: "Experience life in vibrant cities with rich cultural experiences, stunning architecture, and a diverse, welcoming atmosphere.", img: whyNurse1, color: "#FFFBFB" },
      ];
 
