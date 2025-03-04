585f3e39c80a86ff836c9afe907bb16c02a38f23
triCKY05 <6465913@github>
2025-03-04 23:35:15 +0530

    one_section_is_pending

--- src/pages/study-abroad-page.js
+++ src/pages/study-abroad-page.js
@@ -3,6 +3,9 @@ import StudyAbroadHeader from '../components/study-abroad/study_header'
 import WhyStudyAbroad from '../components/study-abroad/whystudyabroad'
 import AbroadDestination from '../components/study-abroad/abroad_destination'
 import IndemandCourse from '../components/study-abroad/indemand-courses/indemand_course'
+import StudyAbroad from '../components/resuable/study-abroad-journey/study-abroad-journey'
+import CountriesWeServe from '../components/resuable/countries-we-serve/countriesWeServe'
+import UniPartner from '../components/resuable/uni-partner/uni-partner'
 
 const StudyAbroadPage = () => {
   return (
@@ -11,6 +14,11 @@ const StudyAbroadPage = () => {
       <WhyStudyAbroad />
       <AbroadDestination />
       <IndemandCourse />
+      <StudyAbroad />
+      <CountriesWeServe />
+      <UniPartner />
+
+
 
     </div>
   )
