a9e3c6460890f6129f36c2d7a18212a108302530
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-03-27 14:09:04 +0530

    fixed

--- src/pages/education-loan.js
+++ src/pages/education-loan.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Header from './components/education-loan/header/header'
-
-
+import React from "react";
+import Header from "../components/education-loan/header/header";
 
 const EducationLoanPage = () => {
-    return (
-        <div>
-            <div className="section-spacin"><Header /></div>
+  return (
+    <div>
+      <div className="section-spacing">
+        <Header />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default EducationLoanPage
+export default EducationLoanPage;
